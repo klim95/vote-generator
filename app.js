db4a5b7512cea50cd06788021e7fc13ad5212cad
KEVIN LIM <KLIM95@UW.EDU>
2015-11-10 12:54:16 -0800

    minor edits

--- app.js
+++ app.js
@@ -3,38 +3,39 @@ var images = []
 function photo(name, path) {
 	this.name = name;
 	this.path = path;
+	this.votes = 0;
 	images.push(this);
 }
 
 var beavisbutthead = new photo ("Beavis and Butthead", "img/beavisbutthead.jpg");
-var bobsburgers = new photo ("Bob's Burgers", "img/bobsburgers");
-var familyguy = new photo ("Family Guy", "img/familyguy");
-var futurama new photo ("Futurama", "img/futurama");
-var johnnybravo new photo ("Johnny Bravo", "img/johnnybravo");
-var kingofthehill new photo ("King of the Hill", "img/kingofthehill");
-var rocketpower new photo ("Rocket Power", "img/rocketpower");
-var scoobydoo new photo ("Scooby Doo", "img/scoobydoo");
-var simpsons new photo ("The Simpsons", "img/simpsons");
-var spongebob new photo ("Spongebob Squarepants", "img/spongebob");
-var tmnt new photo ("Teenage Mutant Ninja Turtles", "img/tmnt");
-var tomjerry new photo ("Tom & Jerry", "img/tomjerry");
+var bobsburgers = new photo ("Bob's Burgers", "img/bobsburgers.jpg");
+var familyguy = new photo ("Family Guy", "img/familyguy.jpg");
+var futurama new photo ("Futurama", "img/futurama.jpg");
+var johnnybravo new photo ("Johnny Bravo", "img/johnnybravo.jpg");
+var kingofthehill new photo ("King of the Hill", "img/kingofthehill.jpg");
+var rocketpower new photo ("Rocket Power", "img/rocketpower.jpg");
+var scoobydoo new photo ("Scooby Doo", "img/scoobydoo.jpg");
+var simpsons new photo ("The Simpsons", "img/simpsons.jpg");
+var spongebob new photo ("Spongebob Squarepants", "img/spongebob.jpg");
+var tmnt new photo ("Teenage Mutant Ninja Turtles", "img/tmnt.jpg");
+var tomjerry new photo ("Tom & Jerry", "img/tomjerry.jpg");
 
 
-var cartoonimage1 = function {
+var cartoon1 = function() {
 	var image1 = Math.floor(Math.random() * images.length);
 	document.getElementById("choice1").src = images[image1].path;
 	document.getElementById("title1").src= images[image1].name;
 }
 
 
-var cartoonimage2 = function {
+var cartoon2 = function() {
 	var image2 = Math.floor(Math.random() * images.length);
 	document.getElementById("choice2").src = images[image2].path;
 	document.getElementById("title2").src - images[image2].name;
 }
 
 photo();
-cartoonimage1();
-cartoonimage2();
+cartoon1();
+cartoon2();
 
 
