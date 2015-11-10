c59be61a6351774a523426ad17d3c491a621db2b
KEVIN LIM <KLIM95@UW.EDU>
2015-11-09 22:55:56 -0800

    updated readme formulated app javascript and edited index html

---
+++ README.md
@@ -0,0 +1,4 @@
+# vote tracker project
+
+This is a program designed for visitors to keep voting for their favorite cartoon out of a selection of two photos that are randomly generated after tracking each vote.
+
--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-function voteGenerator() {
-	
+var images = []
+
+function photo(name, path) {
+	this.name = name;
+	this.path = path;
+	images.push(this);
 }
+
+var beavisbutthead = new photo ("Beavis and Butthead", "img/beavisbutthead.jpg");
+var bobsburgers = new photo ("Bob's Burgers", "img/bobsburgers");
+var familyguy = new photo ("Family Guy", "img/familyguy");
+var futurama new photo ("Futurama", "img/futurama");
+var johnnybravo new photo ("Johnny Bravo", "img/johnnybravo");
+var kingofthehill new photo ("King of the Hill", "img/kingofthehill");
+var rocketpower new photo ("Rocket Power", "img/rocketpower");
+var scoobydoo new photo ("Scooby Doo", "img/scoobydoo");
+var simpsons new photo ("The Simpsons", "img/simpsons");
+var spongebob new photo ("Spongebob Squarepants", "img/spongebob");
+var tmnt new photo ("Teenage Mutant Ninja Turtles", "img/tmnt");
+var tomjerry new photo ("Tom & Jerry", "img/tomjerry");
+
+
+var cartoonimage1 = function {
+	var image1 = Math.floor(Math.random() * images.length);
+	document.getElementById("choice1").src = images[image1].path;
+	document.getElementById("title1").src= images[image1].name;
+}
+
+
+var cartoonimage2 = function {
+	var image2 = Math.floor(Math.random() * images.length);
+	document.getElementById("choice2").src = images[image2].path;
+	document.getElementById("title2").src - images[image2].name;
+}
+
+photo();
+cartoonimage1();
+cartoonimage2();
+
+
